ee9843b26e42d21ec46334c1c602d05c6a56cb7c
dani <danieljuhe@gmail.com>
2022-10-20 15:52:22 +0200

    subiendo ejercicio 11

---
+++ 11-your-first-if.js
@@ -0,0 +1,13 @@
+let total = 54;
+
+function kilometros(){
+    if(total>100){
+        return ("We still have a bit of driving left to go");
+    }else if (total>50){
+        return ("We'll be there in 5 minutes");
+    }else 
+        return ("I'm parking. I'll see you right now");
+    };
+    
+    let answer=kilometros(total);
+    console.log(answer);
