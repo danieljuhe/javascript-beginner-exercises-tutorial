c08bbd94e0ca4f96cdaac44179ebe30d472131ed
dani <danieljuhe@gmail.com>
2022-10-24 12:04:29 +0200

    23 finished

--- 23-bottles-of-milk.js
+++ 23-bottles-of-milk.js
@@ -1,12 +1,18 @@
 function bottles()
-{
-	for(let i = 99; i >=1; i--)
-	{
-		console.log(i);
-	}
-	
+{	let lyrics=""
+	let firstsent=" bottles of milk on the wall, "
+	let secondsent=" bottles of milk."
+	let lastsent="Take one down and pass it around, "
+	for(let i=99; i>=0; i--)
+	{	if(i===99){
+			lyrics=lyrics.concat( i+ firstsent + i + secondsent +"\n"+ lastsent)
+		}else if(i<99 && i>1){
+			lyrics=lyrics.concat(i + firstsent +"\n"+ i + firstsent + i + secondsent +"\n"+ lastsent)
+		}else if(i===1){
+			lyrics=lyrics.concat(i + " bottle of milk on the wall, "+"\n"+ i + " bottle of milk on the wall" + i + " bottle of milk." + "\n" + "No more bottles of milk on the wall"+"\n")
+		}else{
+			lyrics=lyrics.concat("No more bottles of milk on the wall, no more bottles of milk. " +"\n"+"Go to the store and buy some more, 99 bottles of milk on the wall.")
+		}
+	}console.log(lyrics)
 }
-
-
-
 bottles();
