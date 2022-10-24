b331392b5fd51916b14bafe4740eaca77d9b2f46
dani <danieljuhe@gmail.com>
2022-10-24 10:44:22 +0200

    exercise finised & tested

--- 22-the-beatles.js
+++ 22-the-beatles.js
@@ -1,20 +1,20 @@
 let sing = () =>{
-    
-    for(i=0; i<=12; i++){
-        if (i===11){
-            console.log("there will be an answer,")
-        
-        }else if (i===4){
-            console.log("whisper words of wisdom,")
-        
-        }else{
-            console.log("let it be,")
+    let lyrics=""
+    for(let i=0; i<=11; i++){
+        if (i===10){
+           lyrics = lyrics.concat("there will be an answer, ");
         }
-
+        else if (i===4){
+            lyrics = lyrics.concat("whisper words of wisdom, ");
+        
+        }else if (i===11){
+            lyrics = lyrics.concat("let it be");
+        
+        }
+        else{
+            lyrics = lyrics.concat("let it be, ");
         }
     }
-
-let output = parseInt(sing);
-//Your code above ^^^
-
-console.log(output);
+    console.log(lyrics)
+}
+sing()
